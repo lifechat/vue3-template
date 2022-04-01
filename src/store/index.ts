5dab641864c91159451862f9d849639ec86514dd
dengjingjing <dengjingjing@benewtech.cn>
2022-04-01 12:15:16 +0800

    feat():初始化项目配置和请求

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,23 @@
- import {createStore} from 'vuex'
+/*
+ * @Author: your name
+ * @Date: 2020-12-07 18:59:37
+ * @LastEditTime: 2021-08-09 09:57:17
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \blogs-s\src\store\index.ts
+ */
+import {createStore} from 'vuex'
+  
+ const Store = createStore({
+    state: {
+      token: "test",
+      //设置页面是否新窗口打开
+      SetPage: false
+    },
+    mutations: {
+    },
+    actions: {},
+    modules: {}
+})
 
-
- const vuex = createStore({
-     
- });
-
-
- export default vuex;
-
+  export default Store
